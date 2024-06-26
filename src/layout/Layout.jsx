82a027f548614556d04315804f14eb2805f996f4
Nandakumar T <nandakumart07@gmail.com>
2024-06-26 18:29:46 +0530

    route set

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Routers from '../routes/Routers'
+
+function Layout() {
+  return (
+    <>
+    <div>Header</div>
+    <div>sidebar</div>
+    <div><Routers/></div>
+    </>
+  )
+}
+
+export default Layout
