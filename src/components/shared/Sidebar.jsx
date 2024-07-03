53a3eb1e9e1f7528805a010fe89f4d52a412ed85
Nandakumar T <nandakumart07@gmail.com>
2024-07-03 11:59:05 +0530

    sidebar style done

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { FcBullish } from 'react-icons/fc'
-import { DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/navigation'
-import { Link } from 'react-router-dom'
+import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/navigation'
+import { Link, useLocation } from 'react-router-dom'
+import classNames from 'classnames'
+import { HiLogout, HiOutlineLogout } from 'react-icons/hi'
 
-const linkClasses=''
+const linkclasses = `flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base`
 
 function Sidebar() {
   return (
@@ -12,20 +14,28 @@ function Sidebar() {
         <FcBullish fontSize={24} />
         <span className='text-neutral-100 text-lg'>OpenShop</span>
       </div>
-      <div className='flex-1'>
+      <div className='flex-1 py-8 flex flex-col gap-0.5'>
         {DASHBOARD_SIDEBAR_LINKS.map((item) => (
           <SidebarLink key={item.key} item={item} />
         ))}
       </div>
-      <div>bottom part</div>
+      <div className='flex flex-col gap-0.5 border-t border-neutral-700'>
+        {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => (
+          <SidebarLink key={item.key} item={item} />
+        ))}
+        <div className={classNames( 'text-red-500 cursor-pointer', linkclasses)}>
+          <span className='text-xl'><HiOutlineLogout /></span>
+          Logout
+        </div>
+      </div>
     </div>
   )
 }
 
 function SidebarLink({ item }) {
+  const { pathname } = useLocation()
   return (
-    <Link to={item.path} className='flex items-center gap-2 font-light px-3 py-2
-     hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'>
+    <Link to={item.path} className={classNames(pathname === item.path ? 'bg-neutral-500 text-white' : 'text-neutral-400', linkclasses)}>
       <span className='text-xl'>{item.icon}</span>
       {item.label}
     </Link>
