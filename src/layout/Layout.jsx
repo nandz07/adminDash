c5550c9e970b2b474f2800ecbf28a072ecd9bb73
Nandakumar T <nandakumart07@gmail.com>
2024-07-04 18:12:56 +0530

    header done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Routers from '../routes/Routers'
 import Sidebar from '../components/shared/Sidebar'
+import Header from '../components/shared/Header'
 
 function Layout() {
   return (
@@ -8,8 +9,12 @@ function Layout() {
       <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
 
         {/* <div className='bg-sky-200'>Header</div> */}
-        <Sidebar/>
-        <div className='p-4'><Routers /></div>
+        <Sidebar />
+        <div className='flex-1'>
+          <Header />
+          <div className='p-4'><Routers /></div>
+
+        </div>
         {/* <div>Footer</div> */}
       </div>
     </>
